4bd04c2373cc18623746e2e2ab088e2b29bfdc24
Roberto Umbelino <beto.umbelino@gmail.com>
2021-10-16 18:00:45 -0300

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 import { PrismaService } from './prisma.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
 
   const prismaService = app.get(PrismaService)
   prismaService.enableShutdownHooks(app)
